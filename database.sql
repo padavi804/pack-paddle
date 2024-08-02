65b93cb2a58fa883955d32eec535414a7a82656b
Patrick Davison <davisonpatrickj@gmail.com>
2024-08-02 09:27:38 -0500

    initial setup

--- database.sql
+++ database.sql
@@ -2,6 +2,9 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
+--Database name "paddle"
+
+
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
