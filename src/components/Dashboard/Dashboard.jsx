4e67fb8b4d897db85892c7d890e6af22adc6a06c
Patrick Davison <davisonpatrickj@gmail.com>
2024-08-09 14:59:05 -0500

    add to dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -19,32 +19,30 @@ function Dashboard() {
   //   fetchPaddlers();
   // }, []);
 
- 
-  //   history.push('/paddlers');
-  // }
-
   return (
     <div className="container">
       <h2>Greetings, {user.username}!</h2>
-      
-        <h2>Gear List</h2>
-        <label htmlFor="entrypoint">Add Paddlers</label>
-        <br />
-        <form>
-        <input placeholder="First Name"></input>
-        <br />
-        <input placeholder="Last Name"></input>      
+
+      <h2>Dashboard</h2>
+
       <br />
+
       <div className='toGear'>
         <button
           type="button"
-          // onClick={(e) => history.push('/gear')}
-        >Add to List
-        </button>        
-      </div> 
-      </form>
+          onClick={() => history.push('/gear')}
+        >Add to Gear List
+        </button>
+      </div>
+      <br />
+      <div className='toMeal'>
+        <button
+          type="button"
+          onClick={() => history.push('/meal')}
+        >Add to Meal List
+        </button>
+      </div>
     </div>
-   
   );
 }
 
