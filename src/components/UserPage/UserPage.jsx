e8fc7723f9eabcf29d16e88f473ef6be6af040bf
Patrick Davison <davisonpatrickj@gmail.com>
2024-08-20 14:14:40 -0500

    mui trip button on user page

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import {useSelector} from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import './UserPage.css'
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
@@ -13,15 +15,21 @@ function UserPage() {
     <div className="container">
       <h2>Welcome, {user.username}!</h2>
       <p>Your ID is: {user.id}</p>
+      <p>Click on the trips button below to being planning your adventure!</p>
       {/* <LogOutButton className="btn" /> */}
-      <button
+      <Button className="btn" variant="contained" type="button" 
+      sx={{backgroundColor: '#718060'}}
+          onClick={() => {
+            history.push('/trips');
+          }}>Trips</Button>
+      {/* <button
           type="button"
           onClick={() => {
             history.push('/trips');
           }}
         >
           Trips
-        </button>
+        </button> */}
     </div>
   );
 }
