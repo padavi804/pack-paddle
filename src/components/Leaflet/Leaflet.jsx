07584eda18069de75e91f896ad17778c65dbb6f3
Patrick Davison <davisonpatrickj@gmail.com>
2024-10-15 07:02:33 -0500

    adding leaflet boilerplate

---
+++ src/components/Leaflet/Leaflet.jsx
@@ -0,0 +1,21 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import axios from 'axios';
+import * as React from 'react';
+import './GearList.css';
+
+
+function Leaflet() {
+ 
+  
+
+  return (
+    <div id="map" className="map">
+ 
+    </div>
+
+  );
+}
+
+export default Leaflet;
