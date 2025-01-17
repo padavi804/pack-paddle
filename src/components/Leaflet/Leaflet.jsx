d89ccb7a95e9e127c0adfa61be120da070d627a5
Patrick Davison <davisonpatrickj@gmail.com>
2025-01-17 07:05:08 -0600

    Integrate Leaflet for map functionality

--- src/components/Leaflet/Leaflet.jsx
+++ src/components/Leaflet/Leaflet.jsx
@@ -6,14 +6,31 @@ import * as React from 'react';
 import './Leaflet.css';
 import L from 'leaflet';
 
-
 function Leaflet({ }) {
+
+// const position = [51.505, -0.09]
+        
+// render(
+//   <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+//     <TileLayer
+//       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//     />
+//     <Marker position={position}>
+//       <Popup>
+//         A pretty CSS3 popup. <br /> Easily customizable.
+//       </Popup>
+//     </Marker>
+//   </MapContainer>
+// )
+
+
 
   const mapRef = useRef(null);
 
   useEffect(() => {
     // Initialize the Leaflet map when the component mounts
-    const map = L.map(mapRef.current).setView([51.505, -0.09], 13); // Use proper coordinates (lat, lon)
+    const map = L.map(mapRef.current).setView([47.869870, -90.885780], 13); // Use proper coordinates (lat, lon)
 
     // Add OpenStreetMap tile layer to the map
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
